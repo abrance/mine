8f5ff888afa1a31547e71d578bd3a710c0a09050
abrance <1103098607@qq.com>
2020-09-01 17:48:21 +0800

    上传今日份代码，创建测试文件夹和文件

--- w8/w8_31/create_doc_dir.py
+++ w8/w8_31/create_doc_dir.py
@@ -1,37 +1,77 @@
+import random
+import shutil
 import sys
 import os
 
-
+c = y = s = f = 0
+# 2020/9/1 分别表示 case_no, year, store_year, store_time(years) 的个数
 factor_dc = {
-    ''
+    'c': c,
+    'y': y,
+    's': s,
+    'f': f,
+    'e': []
 }
 
 
 def parse(args_ls):
-    for arg in args_ls:
-        pass
+    try:
+        for arg in args_ls:
+            op, value = arg.split('=')
+            if op in factor_dc.keys():
+                factor_dc[op] = int(value)
+            else:
+                factor_dc['e'].append(op)
+        return factor_dc
+    except Exception as e:
+        print(e)
+        return False
 
 
-def _mkdir(dir_name, case_no):
-    # 2020/8/31 dir_name 已存在，case_no 未存在
+def _mkdir(p_dir, k, cnt):
+    dc = {
+        'c': '{}'.format(300+cnt),
+        'y': '{}'.format(1990+cnt),
+        's': '{}'.format(random.choice((0, 30, -1))),
+        'f': 'test-{}.pdf'.format(cnt+1)
+    }
+    if k in dc.keys():
+        v = dc[k]
 
-
-def mkdir(case_cnt: int):
-    parse()
-
-    _dir = os.path.abspath('./')
-    cnt = 0
-    for i in range(100):
-        case_no = '30{}'.format(i)
-        __dir = '/'.join((_dir, case_no))
-        if os.path.exists(__dir):
-            pass
-        else:
+        for i in range(cnt):
+            dir_name = v
+            __dir = '/'.join((p_dir, dir_name))
+            if os.path.exists(__dir):
+                shutil.rmtree(__dir)
             os.mkdir(__dir)
+        return True
+    else:
+        return False
 
-        cnt += 1
-        if cnt == case_cnt:
-            break
+
+def mk_tree(__c, __y, __s, __f, __e, kwargs):
+    # 2020/9/1 组装成一个目录树
+    pwd = os.path.abspath('./')
+    ret = _mkdir(pwd, 'c', __c)
+    if ret:
+        _mkdir()
+
+
+def mkdir(f_dc: dict):
+    _c, _y, _s, _f, _e = f_dc.get('c'), f_dc.get('y'), f_dc.get('s'), f_dc.get('f'), f_dc.get('e')
+    kwargs = {}
+    if _e:
+        try:
+            # 2020/9/1 处理 其它参数逻辑， 保证良好扩展性
+            for item in _e:
+                if str(item).startswith('more_level'):
+                    k, v = item.split('=')
+                    kwargs.__setitem__(k, v)
+
+            mk_tree(_c, _y, _s, _f, _e, kwargs)
+        except Exception as e:
+            print(e)
+            return False
 
 
 def mk_file():
@@ -51,16 +91,16 @@ kw_dc = {
 
 def _input():
     args = sys.argv[1:]
-    keyword = sys.argv[1]       # 2020/8/31 关键字
+    keyword = sys.argv[1]  # 2020/8/31 关键字
 
-    factors = args[1:]          # 2020/8/31 其它的因子
+    factors = args[1:]  # 2020/8/31 其它的因子
     func = kw_dc.get(keyword)
+    parse(factors)
     if func:
-        func(factors)
+        func(factor_dc)
     else:
-        print('* ERROR * '*10)
+        print('* ERROR * ' * 10)
         sys.exit(-1)
-
 
 
 if __name__ == '__main__':
---
+++ w9/w9_1/_lambda.py
@@ -0,0 +1,9 @@
+
+f = lambda x: x+1
+
+t = lambda x: [i for i in x]
+
+
+if __name__ == '__main__':
+    print(f(1))
+    print(t([1]))
