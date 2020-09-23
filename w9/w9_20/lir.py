4e865b35865052f2a7cb68131bea9304c06b377b
abrance <1103098607@qq.com>
2020-09-23 23:30:16 +0800

    lru lirs算法的简单实现

--- w9/w9_20/lir.py
+++ w9/w9_20/lir.py
@@ -1,12 +1,15 @@
+import threading
+import time
 from collections import OrderedDict
 
 
-class LRUCache(object):
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        if not (isinstance(capacity, int) and capacity > 0):
-            raise Exception
+class LRUCache(threading.Thread):
+    """不能存储可变类型对象，不能并发访问set()"""
+    def __init__(self, capacity, auto_pop=False):
         self.capacity = capacity
+        self.cache = OrderedDict()
+        self.auto_pop = auto_pop
+        super(LRUCache, self).__init__()
 
     def get(self, key):
         if key in self.cache:
@@ -16,11 +19,35 @@ class LRUCache(object):
             value = None
         return value
 
-    def set(self, k, v):
-        if self.capacity <= len(self.cache):
-            self.cache.popitem(last=False)
-        self.cache.__setitem__(k, v)
+    def set(self, key, value):
+        if key in self.cache:
+            value = self.cache.pop(key)
+            self.cache[key] = value
+        else:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last=False)  # pop出第一个item
+                self.cache[key] = value
+            else:
+                self.cache[key] = value
+
+    def pop(self, key):
+        if key in self.cache:
+            self.cache.pop(key, -1)
         return True
+
+    def add_job(self):
+        while True:
+            if self.cache:
+                self.cache.popitem()
+                time.sleep(15)
+            else:
+                time.sleep(60)
+
+    def run(self) -> None:
+        if self.auto_pop:
+            self.add_job()
+        else:
+            pass
 
 
 class LIRSCache(object):
